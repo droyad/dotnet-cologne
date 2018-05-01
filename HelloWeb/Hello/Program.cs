9b4e945a686c339e4b825524964c4f596c29f5ed
Robert Wagner <robert.wagner@octopus.com>
2018-05-01 19:07:17 +1000

    Listened on port 80

--- HelloWeb/Hello/Program.cs
+++ HelloWeb/Hello/Program.cs
@@ -20,6 +20,7 @@ namespace Hello
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://*")
                 .Build();
     }
 }
