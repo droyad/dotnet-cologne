d386e5523637727e99b2afcaa67cf777a2e2b159
Robert Wagner <robert.wagner@octopus.com>
2018-05-01 15:00:01 +1000

    Added dotnet core project

---
+++ HelloWeb/Hello/Hello/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hello
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            app.Run(async (context) =>
+            {
+                var name = configuration["name"];
+                context.Response.ContentType = "text/plain; charset=utf-8";
+                await context.Response.WriteAsync($"Griaß di {name} und Köln\r\nI am running on {Environment.MachineName}\r\nVersion {typeof(Startup).Assembly.GetName().Version}", Encoding.UTF8);
+            });
+        }
+    }
+}
